3e8aea26b905a4af10cd6d0ef5233ffba3b13355
Rafael Macchi <rafaelmacchi199@gmail.com>
2024-02-07 01:13:31 -0300

    Starting the project home page

---
+++ src/components/note-card.tsx
@@ -0,0 +1,17 @@
+export function NoteCard() {
+  return (
+    <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+      <span
+        className="text-sm font-medium text-slate-300"
+      >
+        há 2 dias
+      </span>
+      <p className=" text-sm leading-6 text-slate-400">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id explicabo aliquid saepe laboriosam praesentium doloribus ad vitae et reprehenderit fuga totam voluptatibus incidunt rem optio vero, quos iste culpa ipsam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, aperiam quod! Officiis porro iusto sunt sequi labore sed sint, in, deleniti asperiores iste impedit tenetur aperiam id inventore dolor? Neque! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente hic a praesentium fugiat dicta dolor temporibus consequatur repudiandae ut, consectetur exercitationem ipsam id amet laboriosam ad sed, impedit enim corrupti?
+      </p>
+
+      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+
+    </div>
+  )
+}
